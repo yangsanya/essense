0bf1ef2a7208ba2ca992fd7408c354617efd3b88
yangsanya <zdarova4el@mail.ru>
2021-08-05 20:56:02 +0500

    updated rel posts

--- essense/blog/views.py
+++ essense/blog/views.py
@@ -24,4 +24,6 @@ class PostView(DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        context['related_posts'] = list(reversed(Post.objects.all()))[:4]
+
         return context
